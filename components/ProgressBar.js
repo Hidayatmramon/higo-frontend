340d343b85d2b4774cff8d314ec9a5fa24c94883
Ramon hidayat <74350696+Hidayatmramon@users.noreply.github.com>
2025-07-13 11:55:29 +0700

    Update ProgressBar.js

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -7,7 +7,7 @@ export default function ProgressBar() {
   useEffect(() => {
     const interval = setInterval(async () => {
       try {
-        const res = await axios.get('http://localhost:5000/api/customers/progress');
+        const res = await axios.get('https://backend.hidayatmrmon.me/api/customers/progress');
         setProgress(res.data);
       } catch (err) {
         console.error('Gagal ambil progress:', err.message);
